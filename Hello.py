1545f0a214dd9468cce5023ee1aa5c7b944aed77
John Joo <john.joo@dominodatalab.com>
2018-01-27 02:15:46 +0000

    blah

--- Hello.py
+++ Hello.py
@@ -2,5 +2,5 @@ name = raw_input("Who are you? ")
 color = raw_input("What's your favorite color?")
 hometown = raw_input("hometown?"
 print "Hello, " + name + "who likes" + color +"from" +hometown+"."
-# another change
+# another change1
 # change
